41721ae131c6aa6f85cc0e3b465ce0ad91a2ce8f
ZenitsuID #M•R•T™ <85629049+ZenitsuID@users.noreply.github.com>
2021-12-26 02:43:36 +0700

    initial commit

--- EmiliaRobot/modules/admin.py
+++ EmiliaRobot/modules/admin.py
@@ -27,51 +27,7 @@ from EmiliaRobot.modules.helper_funcs.extraction import (
 from EmiliaRobot.modules.log_channel import loggable
 from EmiliaRobot.modules.helper_funcs.alternate import send_message
 
-def Emilia_asu_callback(update, context):
-    query = update.callback_query
-    if query.data == "Emilia_":
-        query.message.edit_text(
-            text="๏ I'm *Emilia*, a powerful group management bot built to help you manage your group easily."
-            "\n• I can restrict users."
-            "\n• I can greet users with customizable welcome messages and even set a group's rules."
-            "\n• I have an advanced anti-flood system."
-            "\n• I can warn users until they reach max warns, with each predefined actions such as ban, mute, kick, etc."
-            "\n• I have a note keeping system, blacklists, and even predetermined replies on certain keywords."
-            "\n• I check for admins' permissions before executing any command and more stuffs"
-            "\n\n_Emilia's licensed under the GNU General Public License v3.0_"
-            "\n\n Click on button bellow to get basic help for Emilia.",
-            parse_mode=ParseMode.MARKDOWN,
-            disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton(
-                            text="Admins​", callback_data="Emilia_admin"
-                        ),
-                    [
-                        InlineKeyboardButton(
-                            text="Back​", callback_data="Emilia_back"
-                        ),
-                    ],
-                ]
-            ),
-        )
-    elif query.data == "Emilia_back":
-        first_name = update.effective_user.first_name
-        uptime = get_readable_time((time.time() - StartTime))
-        query.message.edit_text(
-            PM_START_TEXT.format(
-                escape_markdown(first_name),
-                escape_markdown(uptime),
-                sql.num_users(),
-                sql.num_chats(),
-            ),
-            reply_markup=InlineKeyboardMarkup(buttons),
-            parse_mode=ParseMode.MARKDOWN,
-            timeout=60,
-            disable_web_page_preview=True,
-        )
-             
+
 @bot_admin
 @user_admin
 def set_sticker(update: Update, context: CallbackContext):
