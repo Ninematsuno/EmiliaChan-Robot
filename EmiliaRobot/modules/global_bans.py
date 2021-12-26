9ea9020b36007b58c578693dabd349e7b89c1447
ZenitsuID #M•R•T™ <85629049+ZenitsuID@users.noreply.github.com>
2021-12-26 14:01:48 +0700

    Update !

--- EmiliaRobot/modules/global_bans.py
+++ EmiliaRobot/modules/global_bans.py
@@ -537,19 +537,11 @@ def __chat_settings__(chat_id, user_id):
     return f"This chat is enforcing *gbans*: `{sql.does_chat_gban(chat_id)}`."
 
 
-__help__ = f"""
-*Admins only:*
-❂ `/antispam <on/off/yes/no>`*:* Will toggle our antispam tech or return your current settings.
+__help__ = """
+Antispam is used by the bot owners to ban spammers across all groups. This helps protect you and your groups by removing spam flooders as quickly as possible. This is enabled by default, but you can change this by using the command.
 
-Anti-Spam, used by bot devs to ban spammers across all groups. This helps protect \
-you and your groups by removing spam flooders as quickly as possible.
-*Note:* Users can appeal gbans or report spammers at @{SUPPORT_CHAT}
-
-This also integrates @Spamwatch API to remove Spammers as much as possible from your chatroom!
-*What is SpamWatch?*
-SpamWatch maintains a large constantly updated ban-list of spambots, trolls, bitcoin spammers and unsavoury characters[.](https://telegra.ph/file/f584b643c6f4be0b1de53.jpg)
-Constantly help banning spammers off from your group automatically So, you wont have to worry about spammers storming your group.
-*Note:* Users can appeal spamwatch bans at @SpamwatchSupport
+*Admin only:*
+- /antispam <on/off/yes/no>*:* Change antispam security settings in the group, or return your current settings(when no arguments).
 """
 
 GBAN_HANDLER = CommandHandler("gban", gban, run_async=True)
--- EmiliaRobot/modules/google.py
+++ EmiliaRobot/modules/google.py
@@ -279,15 +279,16 @@ async def apk(e):
 __mod_name__ = "Search"
 
 __help__ = """
-❂ /google <query>*:* Perform a google search
-❂ /image <query>*:* Search Google for images and returns them\nFor greater no. of results specify lim, For eg: `/img hello lim=10`
-❂ /app <appname>*:* Searches for an app in Play Store and returns its details.
-❂ /reverse: Does a reverse image search of the media which it was replied to.
-❂ /gps <location>*:* Get gps location.
-❂ /github <username>*:* Get information about a GitHub user.
-❂ /country <country name>*:* Gathering info about given country
-❂ /imdb <Movie name>*:* Get full info about a movie with imdb.com
-❂ Emilia <query>*:* Emilia answers the query
+- /google <query>*:* Perform a google search
+- /image <query>*:* Search Google for images and returns them\nFor greater no. of results specify lim, For eg: `/img hello lim=10`
+- /app <appname>*:* Searches for an app in Play Store and returns its details.
+- /reverse: Does a reverse image search of the media which it was replied to.
+- /gps <location>*:* Get gps location.
+- /github <username>*:* Get information about a GitHub user.
+- /country <country name>*:* Gathering info about given country.
+- /imdb <Movie name>*:* Get full info about a movie with imdb.com.
+
+Emilia <query>*:* Emilia answers the query.
 
   💡Ex: `Emilia where is Japan?`
 """
