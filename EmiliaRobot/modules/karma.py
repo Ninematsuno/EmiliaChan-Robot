cf8ce9ad77af1726822e1a1b8d98288386533b17
ZenitsuID #M•R•T™ <85629049+ZenitsuID@users.noreply.github.com>
2021-12-27 14:29:50 +0700

    Update karma.py

--- EmiliaRobot/modules/karma.py
+++ EmiliaRobot/modules/karma.py
@@ -197,3 +197,21 @@ async def captcha_state(_, message):
         await message.reply_text("Disabled karma system.")
     else:
         await message.reply_text(usage)
+        
+        __help__ = """
+
+Upvote - Use upvote keywords like "+", "+1", "thanks", etc. to upvote a message.
+
+Downvote - Use downvote keywords like "-", "-1", etc. to downvote a message.
+
+*Commands:*
+
+- /karma: reply to a user to check that user's karma points.
+
+- /karma: send without replying to any message to check karma point list of top 10
+
+- /karmas [off/on]: Enable/Disable karma in your group.
+
+"""
+
+__mod_name__ = "Karma"
